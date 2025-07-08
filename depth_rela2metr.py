8cb2684726b932e3bca73063e5201418eec4a211
Chinartist <1430303711@qq.com>
2025-07-08 14:35:49 +0800

    auto commit: 2025-07-08 14:35:49

--- depth_rela2metr.py
+++ depth_rela2metr.py
@@ -5,11 +5,13 @@ def rela2metr(metr_depth_batch, rela_depth_batch,masks=None):
         metr_depth_array = metr_depth_batch[i]
         rela_depth_array = rela_depth_batch[i]
         metr_depth_array_invert = 1 / metr_depth_array # invert absolute depth with meters
-        x = rela_depth_array.copy().flatten() #rela Depth
-        y = metr_depth_array_invert.copy().flatten()  # metr invert Depth
+        x = rela_depth_array.copy() #rela Depth
+        y = metr_depth_array_invert.copy()  # metr invert Depth
         if masks is not None:
             x = x[masks[i]]
             y = y[masks[i]]
+        x = x.flatten()
+        y = y.flatten()
         A = np.vstack([x, np.ones(len(x))]).T
         s, t = np.linalg.lstsq(A, y, rcond=None)[0]
         rela_depth_aligned_invert = rela_depth_array * s + t
